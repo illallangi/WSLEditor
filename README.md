7bac10dfba9e3845c8595c1e6c51bf10d9b470e7
Andrew Cole <andrew.cole@illallangi.com>
2020-09-02 00:50:52 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# WSLEditor
+[![Docker Pulls](https://img.shields.io/docker/pulls/illallangi/wsleditor.svg)](https://hub.docker.com/r/illallangi/wsleditor)
+[![Image Size](https://images.microbadger.com/badges/image/illallangi/wsleditor.svg)](https://microbadger.com/images/illallangi/wsleditor)
+![Build](https://github.com/illallangi/WSLEditor/workflows/Build/badge.svg)
+
+TODO: SET DESCRIPTION
+
+## Installation
+
+TODO: DOCUMENT INSTALLATION
+
+## Usage
+
+TODO: DOCUMENT USAGE
---
+++ wsleditor/__main__.py
@@ -0,0 +1,14 @@
+import click
+import subprocess
+
+@click.command()
+@click.argument('filename', type=click.Path(file_okay=True, dir_okay=False, writable=True, readable=True, resolve_path=True, allow_dash=False), required=True)
+@click.option('--distribution', envvar='WSL_DISTRO_NAME')
+@click.option('--editor', envvar='WINDOWS_EDITOR', default='/mnt/c/Program Files (x86)/Notepad++/notepad++.exe')
+@click.option('--editor-args', envvar='WINDOWS_EDITOR_ARGS', default='-multiInst -notabbar -nosession -noPlugin')
+def main(filename, distribution, editor, editor_args):
+    windowsFilename = f"\\\\wsl$\\{distribution}{filename}".replace('/', '\\')
+    subprocess.call([editor, *editor_args.split(' '), windowsFilename])
+    
+if __name__ == "__main__":
+  main()
